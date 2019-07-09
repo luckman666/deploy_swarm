9f4107ec5b7c89c29932484c1abdaa4f274716e8
DevOps的那些事 <33419312+luckman666@users.noreply.github.com>
2019-07-09 23:31:04 +0800

    Update deploy_swarm_master.sh

--- deploy_swarm_master.sh
+++ deploy_swarm_master.sh
@@ -150,7 +150,7 @@ systemctl restart docker.service
 
 
 deploy_portainer(){
-docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer	
+docker run -d -p 9000:9000 --name=portainer --restart=always  -v /opt/portainer_data:/data -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer	
 }
 
 
